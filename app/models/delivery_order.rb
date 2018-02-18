b4ee43f0f00057d34d03a7c5005645cb02d87b63
Lim Jiechao <jiechao.lim@gmail.com>
2018-02-18 21:52:35 +0800

    creating_serving_datetime creation in DeliveryOrder model
    Signed-off-by: Lim Jiechao <jiechao.lim@gmail.com>

--- app/models/delivery_order.rb
+++ app/models/delivery_order.rb
@@ -1,12 +1,18 @@
 class DeliveryOrder < ApplicationRecord
   has_many :order_items
   validates :order_id, uniqueness: true, allow_nil: true
-  after_save :create_order_id
+  after_save :create_order_id, :create_serving_datetime
+
 
   private
-  
+
   def create_order_id
     # Create `order_id` by adding prefix 'GO' to `id`
     update_column(:order_id, self.order_id = "GO#{id}")
+  end
+
+  def create_serving_datetime
+    # Create `serving_datetime`
+    update_column(:serving_datetime, self.serving_datetime = updated_at)
   end
 end
