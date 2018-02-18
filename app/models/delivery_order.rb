d12e786a7773b89bfbc0bb4b2bd44459846f126f
Lim Jiechao <jiechao.lim@gmail.com>
2018-02-18 20:58:14 +0800

    order_id creation in DeliveryOrder model
    Signed-off-by: Lim Jiechao <jiechao.lim@gmail.com>

--- app/models/delivery_order.rb
+++ app/models/delivery_order.rb
@@ -1,4 +1,12 @@
 class DeliveryOrder < ApplicationRecord
   has_many :order_items
-  validates :order_id, uniqueness: true
+  validates :order_id, uniqueness: true, allow_nil: true
+  after_save :create_order_id
+
+  private
+  
+  def create_order_id
+    # Create `order_id` by adding prefix 'GO' to `id`
+    update_column(:order_id, self.order_id = "GO#{id}")
+  end
 end
