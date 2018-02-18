35e7c7c267c51412dd10371f89e7a41b1e4ee30f
Lim Jiechao <jiechao.lim@gmail.com>
2018-02-18 23:18:13 +0800

    seed.rb created per requirement
    Signed-off-by: Lim Jiechao <jiechao.lim@gmail.com>

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,39 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+# Clear the DB
+Rake::Task['db:reset'].invoke
+
+# Create 10 meals
+[
+  'Big Mac Extra Value Meal',
+  'Chicken McNuggets Extra Value Meal',
+  'McSpicy Extra Value Meal',
+  'McWings Extra Value Meal',
+  'Grilled Chicken McWrap Extra Value Meal',
+  'Filet-O-Fish Extra Value Meal',
+  'McChicken Extra Value Meal',
+  'Cheeseburger Extra Value Meal',
+  'Veggie Crunch Burger Extra Value Meal',
+  'Grilled Chicken Sandwich Extra Value Meal'
+].each do |meal|
+  Meal.create(
+    name: meal,
+    description: 'Everything is awesome!'
+  )
+end
+
+# Creates 5 delivery orders that contains any number of order items ranging from 1 to 5
+5.times do
+  @order = DeliveryOrder.create()
+  Random.rand(1..5).times do
+    OrderItem.create(
+      delivery_order_id: @order.id,
+      meal_id: Meal.all.sample.id,
+      quantity: Random.rand(1..50),
+      unit_price: Random.rand(500..1000)
+    )
+  end
+end
