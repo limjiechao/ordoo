9fc9f9027a9cc3fb281adea4b214acb1363b2266
Lim Jiechao <jiechao.lim@gmail.com>
2018-02-20 11:05:40 +0800

    minor correction

--- app/controllers/index_controller.rb
+++ app/controllers/index_controller.rb
@@ -3,7 +3,7 @@ class IndexController < ApplicationController
     render json: {
       routes: {
         display_all_orders: 'https://protected-forest-66114.herokuapp.com/orders',
-        display_one_order: 'https://protected-forest-66114.herokuapp.com/orders/:id'
+        display_one_order: 'https://protected-forest-66114.herokuapp.com/orders/:order_id'
       }
     }
   end
